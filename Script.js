cb9f9276c4f867337e916eaef1635cac13d2c329
bgatuy <takiyafathur11@gmail.com>
2025-06-29 08:39:33 +0700

    update

--- Script.js
+++ Script.js
@@ -39,6 +39,7 @@ function extractFlexibleBlock(lines, startLabel, stopLabels = []) {
                .trim();
 }
 
+
 fileInput.addEventListener('change', async function () {
   const file = fileInput.files[0];
   if (!file || file.type !== 'application/pdf') return;
